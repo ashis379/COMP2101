9ce1e70df9afac315815dcd6b4fe460c36af6a61
Ashish Niroula <scor.an379@gmail.com>
2023-06-30 20:38:21 -0400

    Completed sysinfo.sh script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,26 +2,6 @@
 #
 # sysinfo.sh - a script to display information about a computer
 
-# Output template
-output_template="
-Report for $(hostname)
-===============
-
-FQDN: $(hostname -f)
-Operating System name and version: $(lsb_release -ds | cut -d ' ' -f 1,2)
-IP Address: $(ip route get 8.8.8.8 | awk '{print $7}')
-Root Filesystem Free Space: $(df -h / | awk 'NR==2 {print $4}')
-
-===============
-"
-
-# Display the output
-echo "$output_template"
-
-
-
-
-
 # Checking whether the user has root privileges or not.
 if [ "$(id -u)" -ne 0 ]; then
   echo "This script requires root privileges. Please run it as root or using sudo."
@@ -115,14 +95,4 @@ echo
 echo "Operating System Information"
 distro=$(lsb_release -ds)
 if [ -z "$distro" ]; then
-  echo "  Distribution: Data unavailable"
-else
-  echo "  Distribution: $distro"
-fi
 
-version=$(lsb_release -rs)
-if [ -z "$version" ]; then
-  echo "  Version: Data unavailable"
-else
-  echo "  Version: $version"
-fi
